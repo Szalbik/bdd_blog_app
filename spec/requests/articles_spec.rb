4bc76288f7ed8356a3439565e540e4bcc079f61d
Szalbik <szalbierz.d.k@gmail.com>
2017-02-01 20:47:33 +0100

    Fix feature for non exist article

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,25 +1,22 @@
 require 'rails_helper'
-
 RSpec.describe "Articles", type: :request do
   before do
-    @article = Article.create(title: "Title one", description: "This is description of first article")
+    @article = Article.create(title: "Title one", description: "Body of article one")
   end
 
   describe 'GET /articles/:id' do
     context 'with existing article' do
-      before { get "articles/#{@article}" }
-      it 'handles existing article' do
+      before { get "/articles/#{@article.id}" }
+      it "handles existing article" do
         expect(response.status).to eq 200
       end
     end
-
-    context 'with non existing article' do
-      before { get 'articles/xxxx' }
-      it 'handles non existing article' do
+    context 'with non-existing article' do
+      before { get "/articles/xxxxx" }
+      it "handles non-existing article" do
         expect(response.status).to eq 302
         flash_message = "The article you are looking for could not be found"
-        flash[:alert] = flash_message
-        expect(current_path).to eq(root_path)
+        expect(flash[:alert]).to eq flash_message
       end
     end
   end
