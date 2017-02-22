2f66f440f9a95d152a848c525cd85ad9ff4903e6
Szalbik <szalbierz.d.k@gmail.com>
2017-02-22 18:41:43 +0100

    restrict access from delete and edit article by owner

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -2,18 +2,40 @@ require 'rails_helper'
 
 RSpec.feature "Show Article" do
   before do
-    john = User.create(email: 'john@example.com', password: 'password')
-    @article = Article.create(title: "This is an Article", description: "Lorem ipsum dolor sit amet", user: john)
+    @john = User.create(email: 'john@example.com', password: 'password')
+    @fred = User.create(email: 'fred@example.com', password: 'password')
+    @article = Article.create(title: "This is an Article", description: "Lorem ipsum dolor sit amet", user: @john)
   end
 
-  scenario "A user shows an Article" do
+  scenario "to non-signed in user hides Edit/Delete links" do
     visit "/"
-
     click_link(@article.title)
-
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.description)
-
     expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "to non-owner in user hides Edit/Delete links" do
+    login_as(@fred)
+    visit "/"
+    click_link(@article.title)
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.description)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "to signed in owner show Edit/Delete links" do
+    login_as(@john)
+    visit "/"
+    click_link(@article.title)
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.description)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
   end
 end
